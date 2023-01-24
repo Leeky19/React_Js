4a220c5bfc6065246b0fa9880b5bcf7c868dcb47
Julien <arzourjulien@gmail.com>
2023-01-24 12:24:52 +0100

    Tableau qui ne s'affiche pas

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,23 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 
 
 function App() {
-  const reptiles = ["alligator", "snake", "lizard"];
+  const expenses = [
+    {id:1, date :new Date(), name : "ToiletPaper", price : 94.12 },
+    {id:2, date :new Date(), name : "New TV", price : 799.49 },
+    {id:3, date :new Date(), name : "Car Insurance", price : 294.67 },
+    {id:4, date :new Date(), name : "New Desk (Wooden)", price : 450 },
+  ] 
   return (
-    <ol>
-    {reptiles.map((reptile) => (
-      <li>{reptile}</li>
-    ))}
-  </ol>
+  <div>
+    <ul>
+      {expenses.map((price) => (
+        <li key={price} >{price}</li>
+       ))}
+    </ul>
+  </div>
     
   );
 }
