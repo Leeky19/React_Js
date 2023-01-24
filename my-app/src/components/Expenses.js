aed805caab8d77e7b8519fba3b33e3cfe157b3b3
Julien <arzourjulien@gmail.com>
2023-01-24 23:27:45 +0100

    Affiche nom et prix de la dépense mais pas la date

--- my-app/src/components/Expenses.js
+++ my-app/src/components/Expenses.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Card from '../Design/Card';
 
+
+const date = new Date(2018, 8, 22);
+
 const Expenses = (props) => {
     return (
         <li>
             <Card className='expenses'>
                 <div className='expense-description'>
-                    <h2>{props.name}</h2>
-                    <div className='expense-price'>{props.amount}€</div>
+                    <h2> Papier </h2>
+                    <div className='expense-price'>{15}€</div>
                 </div>
+                <div className='expense-date__month'>date.toDateString()</div>
             </Card>
         </li>
     );
