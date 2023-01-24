7f4a783183d3d566608fda32ceda803ce794289b
Julien <arzourjulien@gmail.com>
2023-01-24 12:42:42 +0100

    Affiche les dates au bon format

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,10 +5,10 @@ import './App.css';
 
 function App() {
   const expenses = [
-    {id:1, date :Date.now(), name : "ToiletPaper", price : 94.12 },
-    {id:2, date :Date.now(), name : "New TV", price : 799.49 },
-    {id:3, date :Date.now(), name : "Car Insurance", price : 294.67 },
-    {id:4, date :Date.now(), name : "New Desk (Wooden)", price : 450 },
+    {id:1, date :Date(), name : "ToiletPaper", price : 94.12 },
+    {id:2, date :Date(), name : "New TV", price : 799.49 },
+    {id:3, date :Date(), name : "Car Insurance", price : 294.67 },
+    {id:4, date :Date(), name : "New Desk (Wooden)", price : 450 },
   ] 
   return (
   <div>
