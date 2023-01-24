bfee4f7ecac756e8e415c1cbef2bc9a435799ba1
Julien <arzourjulien@gmail.com>
2023-01-24 16:30:52 +0100

    Creation fichier card

--- my-app/src/components/Expenses.js
+++ my-app/src/components/Expenses.js
@@ -1,7 +1,18 @@
+import React from 'react';
+import Card from '../Design/Card';
+
 const Expenses = (props) => {
     return (
-        <div>Expenses</div>
-    )
+        <li>
+            <Card className='expenses'>
+                <Expenses date={new Date(2018, 8, 22)} />
+                <div className='expense-description'>
+                    <h2>{props.name}</h2>
+                    <div className='expense-price'>{props.amount}€</div>
+                </div>
+            </Card>
+        </li>
+    );
 } 
 
 
