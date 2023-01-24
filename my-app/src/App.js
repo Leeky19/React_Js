4c6b06a86c69049b04b36d202ef21082de34f5fe
Julien <arzourjulien@gmail.com>
2023-01-24 15:09:46 +0100

    Format date sans les heures

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,16 +5,16 @@ import './App.css';
 
 function App() {
   const expenses = [
-    {id:1, date :Date() , name : "ToiletPaper", price : 94.12 },
-    {id:2, date :Date(), name : "New TV", price : 799.49 },
-    {id:3, date :Date(), name : "Car Insurance", price : 294.67 },
-    {id:4, date :Date(), name : "New Desk (Wooden)", price : 450 },
+    {id:1, date : new Date(2018, 8, 22) , name : "ToiletPaper", price : 94.12 },
+    {id:2, date : new Date(2018, 8, 22) , name : "New TV", price : 799.49 },
+    {id:3, date : new Date(2018, 8, 22) , name : "Car Insurance", price : 294.67 },
+    {id:4, date : new Date(2018, 8, 22) , name : "New Desk (Wooden)", price : 450 },
   ] 
   return (
   <div>
     <ul>
       {expenses.map((el, index) => {
-       return <li key={index} >{el.name} - {el.price} -{el.date} </li>
+       return <li key={index} >{el.name} - {el.price} -{el.date.toDateString()} </li>
       })}
     </ul>
   </div>
