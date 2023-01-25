b27c0ec36b4e9094b6f939e716360b73556b7cc1
Julien <arzourjulien@gmail.com>
2023-01-25 15:28:48 +0100

    Ajout de css basique

--- my-app/src/components/Expense.js
+++ my-app/src/components/Expense.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Card from '../Design/Card';
+import "./Expense.css";
+
 
 const Expense = ({expense}) => {
     return ( <div className='expenses'>
                 <div className='expense-description'>
-                    <h2 className ='expense h2'> {expense.name}</h2>
+                    <h2 className ='expense-name'> {expense.name}</h2>
                     <div className='expense-price'>{expense.price}€</div>
                 </div>
                 <div className='expense-date'>{expense.date.toDateString()}</div>
